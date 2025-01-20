221ca1feba62c2da862b99ac15a0571c8fac255e
aliyu200 <aliyualhassan280@gmail.com>
2025-01-20 08:18:45 -0500

    userregistrationpage

--- app/views.py
+++ app/views.py
@@ -1,3 +1,25 @@
-from django.shortcuts import render
+from django.shortcuts import render , redirect
+from django.contrib.auth import authenticate , login 
+from django.contrib import messages
+from django.contrib.auth.models import User
+from django.contrib.auth.forms import UserCreationForm
+from .forms import SignUpForm
+from django import forms
 
 # Create your views here.
+
+def register_user(request):
+  form = SignUpForm()
+  if request.method == "POST":
+    form = SignUpForm(request.POST)
+    if form.is_valid():
+      form.save()
+      username = form.clean_data['username']
+      password = form.clean_data['password1']
+      #login user
+      user = authenticate(username=username, password=password)
+      login(request,user)
+      messages.sucess(request, ("whoops! something went wrong, please try again..."))
+      return redirect('register')
+    else: 
+      return render(request, 'register.html', {'form':form})
